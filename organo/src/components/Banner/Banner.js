77a8bfa645aac9a618b428b1f996536c908f030a
luiz.ladeira <luizpauloladeira@hotmail.com>
2023-01-06 15:25:54 -0300

    observacoes do Fragment do react js

--- organo/src/components/Banner/Banner.js
+++ organo/src/components/Banner/Banner.js
@@ -1,10 +1,17 @@
+import { Fragment } from 'react'
 import './Banner.css'
 
-export function Banner(){
+export function Banner() {
     // JSX é como o react lê o return e transforma em elementos do DOM
+    // Pode se usar <Fragment></Fragment> ou <> </>
+    // O Fragment é usado no lugar da div mais não mostra no browser
     return (
-        <header className="banner">
-            <img src="/imagens/banner.png" alt="O Banner principal da página"/>
-        </header>
+        <Fragment>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="O Banner principal da página" />
+            </header>
+        </Fragment>
+
     )
 }
+
